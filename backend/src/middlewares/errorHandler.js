75d475cb00ba99954fe4c97c07fcd65b5c8ebb9f
Maher Ali <mahiralimattummal@gmail.com>
2025-03-09 23:56:19 +0530

    Update errorHandler.js

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -3,12 +3,12 @@ const ErrorManager = (err, req, res, next) => {
   const message = err.message || "internal server error";
   console.log(err);
   const status = err.status || "error";
-
+  console.log();
   return res.status(statusCode).json({
     statusCode,
     message,
     status,
-    data
+    data,
   });
 };
 export default ErrorManager;
